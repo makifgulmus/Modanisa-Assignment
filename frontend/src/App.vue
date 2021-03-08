a5ab703d24774f9bb39022c6f728d822a6d62858
AkifGulmus <makifgulmus@gmail.com>
2021-03-08 17:17:40 +0300

    create dump vue component with no business logic

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <add-item> </add-item>
+  </div>
+</template>
+
+<script>
+import AddItem from "./components/AddItem.vue";
+
+export default {
+  name: "App",
+  components: {
+    AddItem,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/AddItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form>
+      <input
+        id="todoInput"
+        ref="todoInput"
+        v-model="newTodo"
+        placeholder="What to do?"
+        type="text"
+      />
+      <button>
+        Add Item
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: "",
+    };
+  },
+};
+</script>
+
+<style>
+form {
+  display: flex;
+  justify-content: center;
+}
+input {
+  width: 25%;
+  height: 2rem;
+  font-size: 20px;
+}
+input:focus {
+  outline-width: 0;
+}
+</style>
