7ea8fdc4be4fcc9a6f15524f968ea8b58f790b30
AkifGulmus <makifgulmus@gmail.com>
2021-03-12 13:32:14 +0300

    add unit test for third criterion, get red

--- frontend/tests/unit/single-item.spec.js
+++ frontend/tests/unit/single-item.spec.js
@@ -2,6 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import SingleItem from "../../src/components/SingleItem.vue";
 
 describe("todo items existing and rendering correctly", () => {
+  const spy = jest.spyOn(SingleItem.methods, "changeTodoStatus");
   let wrapper;
   beforeEach(() => {
     wrapper = shallowMount(SingleItem, {
@@ -17,6 +18,15 @@ describe("todo items existing and rendering correctly", () => {
 
   it("exists and renders as a single todo item with text and done attributes", () => {
     expect(wrapper.findAll("span").length).toBe(1);
+    const doneBoxes = wrapper.findAll("#checkbox");
+    expect(doneBoxes.length).toBe(1);
+  });
+
+  it("marks item as done when item is clicked", () => {
+    const items = wrapper.findAll("span");
+    expect(items.length).toBe(1);
+    items.trigger("click");
+    expect(spy).toHaveBeenCalledTimes(1);
     const doneBoxes = wrapper.findAll("#checkbox");
     expect(doneBoxes.length).toBe(1);
   });
--- frontend/tests/unit/todo-items.spec.js
+++ frontend/tests/unit/todo-items.spec.js
@@ -4,6 +4,15 @@ require("jest-fetch-mock").enableMocks();
 
 describe("Rendering and functioning correctly", () => {
   const spy = jest.spyOn(TodoItems.methods, "getTodos");
+  jest.mock("axios", () => ({
+    get: Promise.resolve([
+      {
+        text: "Buy some milk",
+        done: false,
+      },
+    ]),
+  }));
+
   let wrapper;
   beforeEach(() => {
     fetch.resetMocks();
@@ -14,7 +23,15 @@ describe("Rendering and functioning correctly", () => {
     wrapper.destroy();
   });
 
-  it("calls the fetch method", () => {
+  it("calls the fetch method", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify([
+        {
+          text: "Buy some milk",
+          done: false,
+        },
+      ])
+    );
     expect(spy).toHaveBeenCalledTimes(1);
   });
 });
